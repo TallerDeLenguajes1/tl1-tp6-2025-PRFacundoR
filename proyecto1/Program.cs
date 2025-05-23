5e66f36ba87bb41ae4d207b6124b09fa08dc6e26
FacundoPeralta <facundoperalta217@gmail.com>
2025-05-23 11:36:21 -0300

    punto 1 hecho

--- proyecto1/Program.cs
+++ proyecto1/Program.cs
@@ -7,3 +7,39 @@ a=10;
 b=a;
 Console.WriteLine("valor de a:"+a);
 Console.WriteLine("valor de b:"+b);
+
+
+Console.WriteLine("ingrese un numero");
+int i = 0;
+string numero = Console.ReadLine();
+
+
+
+
+
+string invertir(int i, int resto)
+{
+    resto = i % 10;
+    string aux1 = i.ToString();
+    string aux2 = resto.ToString();
+    
+    if (i < 10)
+    {
+        return aux1;
+
+    }
+    else
+    {
+
+        return aux2 +invertir(i / 10, resto);
+    }
+    
+
+
+}
+if (i > 0 && int.TryParse(numero, out i))
+{
+    int resto = i % 10;
+    Console.WriteLine("El numero invertido es " + invertir(i, resto));
+
+}
